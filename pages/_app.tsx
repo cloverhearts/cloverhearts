9cbcc4c111e08562cc03e64145e959eb52431f07
cloverhearts <cloverheartsdev@gmail.com>
2022-08-19 01:46:42 +0900

    + issue-4
    - [ ] layout
    - [ ] posts
    - [ ] post with id/title
    - [ ] context style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,18 @@
 import type { AppProps } from "next/app";
+import { createGlobalStyle } from "styled-components";
+import { normalize } from "styled-normalize";
 
+const GlobalStyles = createGlobalStyle`
+  ${normalize}
+  body { background: green };
+`;
 function CloverHeartsBlogRoot({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default CloverHeartsBlogRoot;
